2ce6fdd9e2ff7d7bfc49396e30d24932c773daac
Wendler Eis <wendler_eis@hotmail.com>
2024-10-16 14:37:44 -0300

    feat: add court listener (#3)

--- src/server.ts
+++ src/server.ts
@@ -1,48 +1,19 @@
-import { createServer } from "http";
 import { Server } from "socket.io";
+import { createServer } from "http";
 
 import { registerQueueListeners } from "./listener/joinListener.js";
-
-interface Payload {
-  name: string;
-}
+import { registerCourtListeners } from "./listener/courtListener.js";
 
 const httpServer = createServer();
 
-export const io = new Server(httpServer, {
+const io = new Server(httpServer, {
   cors: { origin: "*" },
 });
-
-const players: string[] = [];
-
-const playersInGame: string[] = [];
 
 io.on("connection", (socket) => {
   registerQueueListeners(io, socket);
 
-  socket.on("join-court", (data: Payload) => {
-    playersInGame.push(data.name);
-
-    io.emit("players-in-game", playersInGame);
-
-    const playerJoiningIndex = players.indexOf(data.name);
-
-    players.splice(playerJoiningIndex, 1);
-
-    io.emit("players-waiting", players);
-  });
-
-  socket.on("left-court", (data: Payload) => {
-    const playerInGameIndex = playersInGame.indexOf(data.name);
-
-    playersInGame.splice(playerInGameIndex, 1);
-
-    io.emit("players-in-game", playersInGame);
-
-    players.push(data.name);
-
-    io.emit("players-waiting", players);
-  });
+  registerCourtListeners(io, socket);
 
   socket.on("disconnect", () => {
     console.log(`User ${socket.id} disconnected`);
