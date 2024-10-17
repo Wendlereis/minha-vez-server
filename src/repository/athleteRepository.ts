827c8667b0663860247f011d3f8983710ffbf310
Wendler Eis <wendler_eis@hotmail.com>
2024-10-16 21:56:48 -0300

    feat: add connection listener

--- src/repository/athleteRepository.ts
+++ src/repository/athleteRepository.ts
@@ -12,8 +12,12 @@ export function add(athlete: Athlete) {
 
 export function remove(id: string) {
   const athleteLeavingIndex = athletes.findIndex(
-    (athlete) => (athlete.id = id)
+    (athlete) => athlete.id === id
   );
+
+  if (athleteLeavingIndex < 0) {
+    return;
+  }
 
   athletes.splice(athleteLeavingIndex, 1);
 }
