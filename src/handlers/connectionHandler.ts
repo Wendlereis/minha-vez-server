5371326c8170a0cde4c6f99eb806c5b50411694c
Wendler Eis <wendler_eis@hotmail.com>
2024-12-28 22:46:12 -0300

    feat: add constant for events

--- src/handlers/connectionHandler.ts
+++ src/handlers/connectionHandler.ts
@@ -4,18 +4,20 @@ import { courtService } from "../services/courtService.js";
 import { queueService } from "../services/queueService.js";
 import { lobbyService } from "../services/lobbyService.js";
 
+import { court, lobby, server } from "./events.js";
+
 export function registerConnectionHandlers(io: Server, socket: Socket) {
   function disconnect() {
     const courtPlayers = courtService.leave(socket.id);
 
-    io.emit("court:list", courtPlayers);
+    io.emit(court.list, courtPlayers);
 
     queueService.leave(socket.id);
 
     const lobbyList = lobbyService.getList();
 
-    io.emit("lobby:list", lobbyList);
+    io.emit(lobby.list, lobbyList);
   }
 
-  socket.on("disconnect", disconnect);
+  socket.on(server.disconnect, disconnect);
 }
---
+++ src/handlers/events.ts
@@ -0,0 +1,17 @@
+export const lobby = {
+  list: "lobby:list",
+  join: "lobby:join",
+  leave: "lobby:leave",
+  preview: "lobby:preview",
+};
+
+export const court = {
+  list: "court:list",
+  join: "court:join",
+  leave: "court:leave",
+};
+
+export const server = {
+  disconnect: "disconnect",
+  connection: "connection",
+};
