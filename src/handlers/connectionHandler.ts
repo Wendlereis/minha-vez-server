7e35bde9202f47d08bd46a44642d586756c599a9
Wendler Eis <wendler_eis@hotmail.com>
2025-02-22 13:52:20 -0300

    ♻️  Refactor courtService (#30)
    * refactor: remove unused return
    
    * chore: remove unused return

--- src/handlers/connectionHandler.ts
+++ src/handlers/connectionHandler.ts
@@ -4,13 +4,11 @@ import { courtService } from "../services/courtService.js";
 import { queueService } from "../services/queueService.js";
 import { lobbyService } from "../services/lobbyService.js";
 
-import { court, lobby, server } from "./events.js";
+import { lobby, server } from "./events.js";
 
 export function registerConnectionHandlers(io: Server, socket: Socket) {
   function disconnect() {
-    const courtPlayers = courtService.leave(socket.id);
-
-    io.emit(court.list, courtPlayers);
+    courtService.leave(socket.id);
 
     queueService.leave(socket.id);
 
